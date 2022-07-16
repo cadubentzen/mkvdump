ca767c36bfada68697de149adfb1e715b1cff17e
Carlos Bentzen <cadubentzen@gmail.com>
2022-07-16 14:37:44 +0200

    Move from main.rs to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod buffer_reader;
+pub mod reader;
+pub mod status;
+
+pub use buffer_reader::BufferReader;
+pub use reader::Reader;
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-mod buffer_reader;
-mod reader;
-mod status;
-
-fn main() {
-    println!("Hello, world!");
-}
