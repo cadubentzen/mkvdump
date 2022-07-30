1e85515466c215a4d7ec3247fc497cee3c129b38
Carlos Bentzen <cadubentzen@gmail.com>
2022-07-30 15:36:42 +0200

    Turn into a binary package

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{self, Read},
+};
 
+use clap::Parser;
 use enum_primitive_derive::Primitive;
 use lazy_static::lazy_static;
 use nom::{
@@ -843,4 +848,55 @@ mod tests {
 
         assert_eq!(build_element_trees(&elements), expected);
     }
+}
+
+/// WebM dump
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the file to be dumped
+    filename: String,
+
+    /// Output in JSON format, rather than the default YAML
+    #[clap(short, long, default_value = "yaml")]
+    format: String,
+}
+
+fn main() -> io::Result<()> {
+    let args = Args::parse();
+    let mut file = File::open(args.filename)?;
+
+    // TODO: read chunked to not load entire video in memory.
+    let mut buffer = Vec::<u8>::new();
+    file.read_to_end(&mut buffer)?;
+
+    let mut elements = Vec::<Element>::new();
+
+    let mut read_buffer = &buffer[..];
+    loop {
+        match parse_element(read_buffer) {
+            Ok((new_read_buffer, element)) => {
+                elements.push(element);
+                if new_read_buffer.is_empty() {
+                    break;
+                }
+                read_buffer = new_read_buffer;
+            }
+            Err(nom::Err::Incomplete(needed)) => {
+                println!(
+                    "Needed: {:?}\nPartial result:\n{}",
+                    needed,
+                    serde_yaml::to_string(&elements).unwrap()
+                );
+                todo!("Partial reads not implemented")
+            }
+            Err(_) => {
+                panic!("Something is wrong");
+            }
+        }
+    }
+
+    print_element_trees(&elements, &args.format);
+
+    Ok(())
 }
