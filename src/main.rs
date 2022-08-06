fded731372f95755d5b8e764843f6fe4ee224787
Carlos Bentzen <cadubentzen@gmail.com>
2022-08-06 13:59:36 +0200

    Use new macro-based ids

--- src/main.rs
+++ src/main.rs
@@ -1,344 +1,21 @@
 use std::{
-    collections::HashMap,
     fs::File,
     io::{self, Read},
 };
 
 use clap::Parser;
-use enum_primitive_derive::Primitive;
-use lazy_static::lazy_static;
 use nom::{
     bytes::streaming::take,
     combinator::peek,
     error::{Error, ErrorKind},
     Err, IResult,
 };
-use num_traits::FromPrimitive;
 use serde::Serialize;
 
-#[derive(Debug, PartialEq)]
-enum Type {
-    Unsigned,
-    Signed,
-    Float,
-    String,
-    Utf8,
-    Date,
-    Master,
-    Binary,
-}
+mod ebml;
+mod elements;
 
-#[repr(u32)]
-#[derive(Debug, Clone, PartialEq, Primitive, Copy, Eq, Hash, Serialize)]
-pub enum Id {
-    Unknown = 0x00,
-    Ebml = 0x1A45DFA3,
-    EbmlVersion = 0x4286,
-    EbmlReadVersion = 0x42F7,
-    EbmlMaxIdLength = 0x42F2,
-    EbmlMaxSizeLength = 0x42F3,
-    DocType = 0x4282,
-    DocTypeVersion = 0x4287,
-    DocTypeReadVersion = 0x4285,
-    Void = 0xEC,
-    Segment = 0x18538067,
-    SeekHead = 0x114D9B74,
-    Seek = 0x4DBB,
-    SeekId = 0x53AB,
-    SeekPosition = 0x53AC,
-    Info = 0x1549A966,
-    TimecodeScale = 0x2AD7B1,
-    Duration = 0x4489,
-    DateUtc = 0x4461,
-    Title = 0x7BA9,
-    MuxingApp = 0x4D80,
-    WritingApp = 0x5741,
-    Cluster = 0x1F43B675,
-    Timecode = 0xE7,
-    PrevSize = 0xAB,
-    SimpleBlock = 0xA3,
-    BlockGroup = 0xA0,
-    Block = 0xA1,
-    BlockVirtual = 0xA2,
-    BlockAdditions = 0x75A1,
-    BlockMore = 0xA6,
-    BlockAddId = 0xEE,
-    BlockAdditional = 0xA5,
-    BlockDuration = 0x9B,
-    ReferenceBlock = 0xFB,
-    DiscardPadding = 0x75A2,
-    Slices = 0x8E,
-    TimeSlice = 0xE8,
-    LaceNumber = 0xCC,
-    Tracks = 0x1654AE6B,
-    TrackEntry = 0xAE,
-    TrackNumber = 0xD7,
-    TrackUid = 0x73C5,
-    TrackType = 0x83,
-    FlagEnabled = 0xB9,
-    FlagDefault = 0x88,
-    FlagForced = 0x55AA,
-    FlagLacing = 0x9C,
-    DefaultDuration = 0x23E383,
-    Name = 0x536E,
-    Language = 0x22B59C,
-    CodecId = 0x86,
-    CodecPrivate = 0x63A2,
-    CodecName = 0x258688,
-    CodecDelay = 0x56AA,
-    SeekPreRoll = 0x56BB,
-    Video = 0xE0,
-    FlagInterlaced = 0x9A,
-    StereoMode = 0x53B8,
-    AlphaMode = 0x53C0,
-    PixelWidth = 0xB0,
-    PixelHeight = 0xBA,
-    PixelCropBottom = 0x54AA,
-    PixelCropTop = 0x54BB,
-    PixelCropLeft = 0x54CC,
-    PixelCropRight = 0x54DD,
-    DisplayWidth = 0x54B0,
-    DisplayHeight = 0x54BA,
-    DisplayUnit = 0x54B2,
-    AspectRatioType = 0x54B3,
-    FrameRate = 0x2383E3,
-    Colour = 0x55B0,
-    MatrixCoefficients = 0x55B1,
-    BitsPerChannel = 0x55B2,
-    ChromaSubsamplingHorz = 0x55B3,
-    ChromaSubsamplingVert = 0x55B4,
-    CbSubsamplingHorz = 0x55B5,
-    CbSubsamplingVert = 0x55B6,
-    ChromaSitingHorz = 0x55B7,
-    ChromaSitingVert = 0x55B8,
-    Range = 0x55B9,
-    TransferCharacteristics = 0x55BA,
-    Primaries = 0x55BB,
-    MaxCll = 0x55BC,
-    MaxFall = 0x55BD,
-    MasteringMetadata = 0x55D0,
-    PrimaryRChromaticityX = 0x55D1,
-    PrimaryRChromaticityY = 0x55D2,
-    PrimaryGChromaticityX = 0x55D3,
-    PrimaryGChromaticityY = 0x55D4,
-    PrimaryBChromaticityX = 0x55D5,
-    PrimaryBChromaticityY = 0x55D6,
-    WhitePointChromaticityX = 0x55D7,
-    WhitePointChromaticityY = 0x55D8,
-    LuminanceMax = 0x55D9,
-    LuminanceMin = 0x55DA,
-    Projection = 0x7670,
-    ProjectionType = 0x7671,
-    ProjectionPrivate = 0x7672,
-    ProjectionPoseYaw = 0x7673,
-    ProjectionPosePitch = 0x7674,
-    ProjectionPoseRoll = 0x7675,
-    Audio = 0xE1,
-    SamplingFrequency = 0xB5,
-    OutputSamplingFrequency = 0x78B5,
-    Channels = 0x9F,
-    BitDepth = 0x6264,
-    ContentEncodings = 0x6D80,
-    ContentEncoding = 0x6240,
-    ContentEncodingOrder = 0x5031,
-    ContentEncodingScope = 0x5032,
-    ContentEncodingType = 0x5033,
-    ContentEncryption = 0x5035,
-    ContentEncAlgo = 0x47E1,
-    ContentEncKeyId = 0x47E2,
-    ContentEncAesSettings = 0x47E7,
-    AesSettingsCipherMode = 0x47E8,
-    Cues = 0x1C53BB6B,
-    CuePoint = 0xBB,
-    CueTime = 0xB3,
-    CueTrackPositions = 0xB7,
-    CueTrack = 0xF7,
-    CueClusterPosition = 0xF1,
-    CueRelativePosition = 0xF0,
-    CueDuration = 0xB2,
-    CueBlockNumber = 0x5378,
-    Chapters = 0x1043A770,
-    EditionEntry = 0x45B9,
-    ChapterAtom = 0xB6,
-    ChapterUid = 0x73C4,
-    ChapterStringUid = 0x5654,
-    ChapterTimeStart = 0x91,
-    ChapterTimeEnd = 0x92,
-    ChapterDisplay = 0x80,
-    ChapString = 0x85,
-    ChapLanguage = 0x437C,
-    ChapCountry = 0x437E,
-    Tags = 0x1254C367,
-    Tag = 0x7373,
-    Targets = 0x63C0,
-    TargetTypeValue = 0x68CA,
-    TargetType = 0x63CA,
-    TagTrackUid = 0x63C5,
-    SimpleTag = 0x67C8,
-    TagName = 0x45A3,
-    TagLanguage = 0x447A,
-    TagDefault = 0x4484,
-    TagString = 0x4487,
-    TagBinary = 0x4485,
-}
-
-// http://www.webmproject.org/docs/container/
-// http://www.webmproject.org/docs/webm-encryption/#42-new-matroskawebm-elements
-// http://matroska.org/technical/specs/index.html
-// TODO: use proc-macros to avoid this lazy_static
-lazy_static! {
-    static ref ID_ELEMENT_TYPE_MAP: HashMap<Id, Type> = HashMap::from([
-        (Id::Unknown, Type::Binary),
-        (Id::Ebml, Type::Master),
-        (Id::EbmlVersion, Type::Unsigned),
-        (Id::EbmlReadVersion, Type::Unsigned),
-        (Id::EbmlMaxIdLength, Type::Unsigned),
-        (Id::EbmlMaxSizeLength, Type::Unsigned),
-        (Id::DocType, Type::String),
-        (Id::DocTypeVersion, Type::Unsigned),
-        (Id::DocTypeReadVersion, Type::Unsigned),
-        (Id::Void, Type::Binary),
-        (Id::Segment, Type::Master),
-        (Id::SeekHead, Type::Master),
-        (Id::Seek, Type::Master),
-        (Id::SeekId, Type::Binary),
-        (Id::SeekPosition, Type::Unsigned),
-        (Id::Info, Type::Master),
-        (Id::TimecodeScale, Type::Unsigned),
-        (Id::Duration, Type::Float),
-        (Id::DateUtc, Type::Date),
-        (Id::Title, Type::Utf8),
-        (Id::MuxingApp, Type::Utf8),
-        (Id::WritingApp, Type::Utf8),
-        (Id::Cluster, Type::Master),
-        (Id::Timecode, Type::Unsigned),
-        (Id::PrevSize, Type::Unsigned),
-        (Id::SimpleBlock, Type::Binary),
-        (Id::BlockGroup, Type::Master),
-        (Id::Block, Type::Binary),
-        (Id::BlockVirtual, Type::Binary),
-        (Id::BlockAdditions, Type::Master),
-        (Id::BlockMore, Type::Master),
-        (Id::BlockAddId, Type::Unsigned),
-        (Id::BlockAdditional, Type::Binary),
-        (Id::BlockDuration, Type::Unsigned),
-        (Id::ReferenceBlock, Type::Signed),
-        (Id::DiscardPadding, Type::Signed),
-        (Id::Slices, Type::Master),
-        (Id::TimeSlice, Type::Master),
-        (Id::LaceNumber, Type::Unsigned),
-        (Id::Tracks, Type::Master),
-        (Id::TrackEntry, Type::Master),
-        (Id::TrackNumber, Type::Unsigned),
-        (Id::TrackUid, Type::Unsigned),
-        (Id::TrackType, Type::Unsigned),
-        (Id::FlagEnabled, Type::Unsigned),
-        (Id::FlagDefault, Type::Unsigned),
-        (Id::FlagForced, Type::Unsigned),
-        (Id::FlagLacing, Type::Unsigned),
-        (Id::DefaultDuration, Type::Unsigned),
-        (Id::Name, Type::Utf8),
-        (Id::Language, Type::String),
-        (Id::CodecId, Type::String),
-        (Id::CodecPrivate, Type::Binary),
-        (Id::CodecName, Type::Utf8),
-        (Id::CodecDelay, Type::Unsigned),
-        (Id::SeekPreRoll, Type::Unsigned),
-        (Id::Video, Type::Master),
-        (Id::FlagInterlaced, Type::Unsigned),
-        (Id::StereoMode, Type::Unsigned),
-        (Id::AlphaMode, Type::Unsigned),
-        (Id::PixelWidth, Type::Unsigned),
-        (Id::PixelHeight, Type::Unsigned),
-        (Id::PixelCropBottom, Type::Unsigned),
-        (Id::PixelCropTop, Type::Unsigned),
-        (Id::PixelCropLeft, Type::Unsigned),
-        (Id::PixelCropRight, Type::Unsigned),
-        (Id::DisplayWidth, Type::Unsigned),
-        (Id::DisplayHeight, Type::Unsigned),
-        (Id::DisplayUnit, Type::Unsigned),
-        (Id::AspectRatioType, Type::Unsigned),
-        (Id::FrameRate, Type::Float),
-        (Id::Colour, Type::Master),
-        (Id::MatrixCoefficients, Type::Unsigned),
-        (Id::BitsPerChannel, Type::Unsigned),
-        (Id::ChromaSubsamplingHorz, Type::Unsigned),
-        (Id::ChromaSubsamplingVert, Type::Unsigned),
-        (Id::CbSubsamplingHorz, Type::Unsigned),
-        (Id::CbSubsamplingVert, Type::Unsigned),
-        (Id::ChromaSitingHorz, Type::Unsigned),
-        (Id::ChromaSitingVert, Type::Unsigned),
-        (Id::Range, Type::Unsigned),
-        (Id::TransferCharacteristics, Type::Unsigned),
-        (Id::Primaries, Type::Unsigned),
-        (Id::MaxCll, Type::Unsigned),
-        (Id::MaxFall, Type::Unsigned),
-        (Id::MasteringMetadata, Type::Master),
-        (Id::PrimaryRChromaticityX, Type::Float),
-        (Id::PrimaryRChromaticityY, Type::Float),
-        (Id::PrimaryGChromaticityX, Type::Float),
-        (Id::PrimaryGChromaticityY, Type::Float),
-        (Id::PrimaryBChromaticityX, Type::Float),
-        (Id::PrimaryBChromaticityY, Type::Float),
-        (Id::WhitePointChromaticityX, Type::Float),
-        (Id::WhitePointChromaticityY, Type::Float),
-        (Id::LuminanceMax, Type::Float),
-        (Id::LuminanceMin, Type::Float),
-        (Id::Projection, Type::Master),
-        (Id::ProjectionType, Type::Unsigned),
-        (Id::ProjectionPrivate, Type::Binary),
-        (Id::ProjectionPoseYaw, Type::Float),
-        (Id::ProjectionPosePitch, Type::Float),
-        (Id::ProjectionPoseRoll, Type::Float),
-        (Id::Audio, Type::Master),
-        (Id::SamplingFrequency, Type::Float),
-        (Id::OutputSamplingFrequency, Type::Float),
-        (Id::Channels, Type::Unsigned),
-        (Id::BitDepth, Type::Unsigned),
-        (Id::ContentEncodings, Type::Master),
-        (Id::ContentEncoding, Type::Master),
-        (Id::ContentEncodingOrder, Type::Unsigned),
-        (Id::ContentEncodingScope, Type::Unsigned),
-        (Id::ContentEncodingType, Type::Unsigned),
-        (Id::ContentEncryption, Type::Master),
-        (Id::ContentEncAlgo, Type::Unsigned),
-        (Id::ContentEncKeyId, Type::Binary),
-        (Id::ContentEncAesSettings, Type::Master),
-        (Id::AesSettingsCipherMode, Type::Unsigned),
-        (Id::Cues, Type::Master),
-        (Id::CuePoint, Type::Master),
-        (Id::CueTime, Type::Unsigned),
-        (Id::CueTrackPositions, Type::Master),
-        (Id::CueTrack, Type::Unsigned),
-        (Id::CueClusterPosition, Type::Unsigned),
-        (Id::CueRelativePosition, Type::Unsigned),
-        (Id::CueDuration, Type::Unsigned),
-        (Id::CueBlockNumber, Type::Unsigned),
-        (Id::Chapters, Type::Master),
-        (Id::EditionEntry, Type::Master),
-        (Id::ChapterAtom, Type::Master),
-        (Id::ChapterUid, Type::Unsigned),
-        (Id::ChapterStringUid, Type::Utf8),
-        (Id::ChapterTimeStart, Type::Unsigned),
-        (Id::ChapterTimeEnd, Type::Unsigned),
-        (Id::ChapterDisplay, Type::Master),
-        (Id::ChapString, Type::Utf8),
-        (Id::ChapLanguage, Type::String),
-        (Id::ChapCountry, Type::String),
-        (Id::Tags, Type::Master),
-        (Id::Tag, Type::Master),
-        (Id::Targets, Type::Master),
-        (Id::TargetTypeValue, Type::Unsigned),
-        (Id::TargetType, Type::String),
-        (Id::TagTrackUid, Type::Unsigned),
-        (Id::SimpleTag, Type::Master),
-        (Id::TagName, Type::Utf8),
-        (Id::TagLanguage, Type::String),
-        (Id::TagDefault, Type::Unsigned),
-        (Id::TagString, Type::Utf8),
-        (Id::TagBinary, Type::Binary),
-    ]);
-}
+use crate::elements::*;
 
 fn parse_id(input: &[u8]) -> IResult<&[u8], Id> {
     let (input, first_byte) = peek(take(1usize))(input)?;
@@ -357,18 +34,11 @@ fn parse_id(input: &[u8]) -> IResult<&[u8], Id> {
     value_buffer[(4 - varint_bytes.len())..].copy_from_slice(varint_bytes);
     let id = u32::from_be_bytes(value_buffer);
 
-    Ok((
-        input,
-        Id::from_u32(id).unwrap_or_else(|| {
-            eprintln!("Failed to parse ID 0x{:X}. Is it supported by WebM?", id);
-            eprintln!("Check here: https://www.webmproject.org/docs/container/");
-            Id::Unknown
-        }),
-    ))
+    Ok((input, Id::new(id)))
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
-pub struct Header {
+struct Header {
     pub id: Id,
     pub header_size: usize,
     #[serde(skip_serializing)]
@@ -434,14 +104,14 @@ fn parse_header(input: &[u8]) -> IResult<&[u8], Header> {
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
 #[serde(untagged)]
-pub enum BinaryValue {
+enum BinaryValue {
     SeekId(Id),
     Hidden,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
 #[serde(untagged)]
-pub enum Body {
+enum Body {
     Master,
     Unsigned(u64),
     Signed(i64),
@@ -453,18 +123,16 @@ pub enum Body {
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
-pub struct Element {
+struct Element {
     #[serde(flatten)]
     pub header: Header,
     #[serde(rename = "value")]
     pub body: Body,
 }
 
-pub fn parse_element(input: &[u8]) -> IResult<&[u8], Element> {
+fn parse_element(input: &[u8]) -> IResult<&[u8], Element> {
     let (input, header) = parse_header(input)?;
-    let element_type = ID_ELEMENT_TYPE_MAP
-        .get(&header.id)
-        .expect("All IDs should have an entry in the lookup map");
+    let element_type = header.id.get_type();
 
     let (input, body) = match element_type {
         Type::Master => Ok((input, Body::Master)),
@@ -610,7 +278,7 @@ fn build_element_trees(elements: &[Element]) -> Vec<ElementTree> {
     trees
 }
 
-pub fn print_element_trees(elements: &[Element], format: &str) {
+fn print_element_trees(elements: &[Element], format: &str) {
     let element_trees = build_element_trees(elements);
     let serialized = if format == "json" {
         serde_json::to_string_pretty(&element_trees).unwrap()
@@ -748,13 +416,13 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_unknown() {
+    fn test_parse_crc32() {
         assert_eq!(
             parse_element(&[0xBF, 0x84, 0xAF, 0x93, 0x97, 0x18]),
             Ok((
                 EMPTY,
                 Element {
-                    header: Header::new(Id::Unknown, 2, 4),
+                    header: Header::new(Id::Crc32, 2, 4),
                     body: Body::Binary(BinaryValue::Hidden)
                 }
             ))
