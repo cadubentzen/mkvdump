3814a06f8fafe1150e102943ddf1c78cdaa466bd
Carlos Bentzen <cadubentzen@gmail.com>
2022-08-08 20:17:32 +0200

    Remove unwanted comment

--- src/ebml.rs
+++ src/ebml.rs
@@ -1,11 +1,3 @@
-// ebml_elements! {
-//     name = EBML, id = 0x1A45DFA3, type = master;
-//     name = EBMLVersion, id = 0x4286, type = uinteger;
-//     name = EBMLReadVersion, id = 0x42F7, type = uinteger;
-//     name = EBMLMaxIDLength, id = 0x42F2, type = uinteger;
-//     name = EBMLMaxSizeLength, id = 0x42F3, type = uinteger;
-// }
-
 macro_rules! ebml_elements {
     ($(name = $element_name:ident, id = $id:expr, variant = $variant:ident;)+) => {
         use serde::Serialize;
