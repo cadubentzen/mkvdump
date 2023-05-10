ba00bcae5efbb76edb23f8f216fca9e3eb8669e1
Carlos Bentzen <cadubentzen@gmail.com>
2023-05-10 08:16:58 +0000

    add docs and cli tests (#47)

---
+++ tests/cli.rs
@@ -0,0 +1,25 @@
+use assert_cmd::Command;
+
+fn command() -> Command {
+    Command::cargo_bin("mkvdump").unwrap()
+}
+
+#[test]
+fn basic() {
+    const INPUT: &str = "inputs/encrypted.hdr";
+    command().arg(INPUT).assert().success();
+    command()
+        .arg("-f")
+        .arg("json")
+        .arg(INPUT)
+        .assert()
+        .success();
+    command()
+        .arg("-f")
+        .arg("yaml")
+        .arg(INPUT)
+        .assert()
+        .success();
+    command().arg("-p").arg(INPUT).assert().success();
+    command().arg("-l").arg(INPUT).assert().success();
+}
