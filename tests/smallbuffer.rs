0b466a5fa03c10f40a6e4d1d27e3ee39b05d2503
Carlos Bentzen <cadubentzen@gmail.com>
2023-05-15 11:47:34 +0200

    add test

--- tests/smallbuffer.rs
+++ tests/smallbuffer.rs
@@ -1,9 +1,19 @@
 use mkvdump::parse_elements_from_file;
 
 #[test]
-fn given_small_buffer_fail_when_element_does_not_fit() {
+fn bail_when_element_does_not_fit() {
     assert_eq!(
         parse_elements_from_file("tests/inputs/matroska-test-suite/test1.mkv", false, 65536)
+            .unwrap_err()
+            .to_string(),
+        "failed to parse the given file with buffer size of 65536 bytes"
+    );
+}
+
+#[test]
+fn bail_when_corrupt_for_longer_than_buffer() {
+    assert_eq!(
+        parse_elements_from_file("tests/inputs/matroska-test-suite/test7.mkv", false, 65536)
             .unwrap_err()
             .to_string(),
         "failed to parse the given file with buffer size of 65536 bytes"
